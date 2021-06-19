51c8aa3f50db26594368ade5d21a06f286bdf12a
Dan <59460714+dan9731@users.noreply.github.com>
2021-06-18 21:18:55 -0400

    Update i_like_to_eat.py
    More pull practice.

--- i_like_to_eat.py
+++ i_like_to_eat.py
@@ -1,1 +1,3 @@
-print("All kinds.. you name it: apple, cherry, pumpkin.. all sup delish.")
+flavors = ['apple', 'cherry', 'pumpkin', ]
+for flavor in flavors:
+  print("I like to eat ", flavor, "pie!")
